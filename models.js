60ab825ccb80ad5748fc691900a85822b011aa1b
Román Solanas <20.12.99.roman@gmail.com>
2023-02-19 23:39:37 +0100

    actualizando modelos

--- models.js
+++ models.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const Cliente = mongoose.model('Cliente',
-  new mongoose.Schema({ Nombre: String, Apellidos: String , Num_Armas: Number})
+  new mongoose.Schema({ _id :Number,Nombre: String, Apellidos: String , Num_Armas: Number})
 );
 
 const Arma = mongoose.model('Armas',
-  new mongoose.Schema({ Nombre: String, Tipo: String })
+  new mongoose.Schema({ _id :Number, Nombre: String, Tipo: String })
 );
 
 module.exports = {
