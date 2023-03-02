90cc01a072e713e80daed7020507c42549c5af34
Román Solanas <20.12.99.roman@gmail.com>
2023-03-02 21:05:32 +0100

    Mod modelos

--- models.js
+++ models.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Cliente = mongoose.model('Cliente',
-  new mongoose.Schema({ Nombre: String, Apellidos: String , Num_Armas: Number})
+const Cliente = mongoose.model(
+  "Cliente",
+  new mongoose.Schema({ nombre: String, apellidos: String, num_armas: Number })
 );
 
-const Arma = mongoose.model('Armas',
-  new mongoose.Schema({  Nombre: String, Tipo: String })
+const Arma = mongoose.model(
+  "Armas",
+  new mongoose.Schema({ nombre: String, tipo: String })
 );
 
 module.exports = {
   Cliente: Cliente,
-  Arma: Arma
-}
+  Arma: Arma,
+};
